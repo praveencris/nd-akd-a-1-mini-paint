c1eb9314e83699190fdd50ed4d4dd9e5de8b6579
Praveen <pkchmits@gmail.com>
2021-01-16 10:20:06 +0530

    Added functionality for moveUp() function

--- app/src/main/java/com/sabkayar/praveen/minipaint/MyCanvasView.kt
+++ app/src/main/java/com/sabkayar/praveen/minipaint/MyCanvasView.kt
@@ -7,6 +7,7 @@ import android.graphics.Paint
 import android.graphics.Path
 import android.view.MotionEvent
 import android.view.View
+import android.view.ViewConfiguration
 import androidx.core.content.res.ResourcesCompat
 
 private const val STROKE_WIDTH = 12f // has to be float
@@ -68,8 +69,22 @@ class MyCanvasView(context: Context) : View(context) {
         TODO("Not yet implemented")
     }
 
+
+    val touchTolerance = ViewConfiguration.get(context).scaledTouchSlop
+
     private fun touchMove() {
-        TODO("Not yet implemented")
+        val dx = Math.abs(motionTouchEventX - currentX)
+        val dy = Math.abs(motionTouchEventY - currentY)
+        if (dx >= touchTolerance || dy >= touchTolerance) {
+            // QuadTo() adds a quadratic bezier from the last point,
+            // approaching control point (x1,y1), and ending at (x2,y2).
+            path.quadTo(currentX, currentY, (motionTouchEventX + currentX) / 2, (motionTouchEventY + currentY) / 2)
+            currentX = motionTouchEventX
+            currentY = motionTouchEventY
+            // Draw the path in the extra bitmap to cache it.
+            extraCanvas.drawPath(path, paint)
+        }
+        invalidate()
     }
 
     private fun touchStart() {
